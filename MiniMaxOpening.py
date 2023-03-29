c59587adc3957c8a2b67d70567e24f6314a6e03e
pratika <pratika@verveware.co>
2023-03-29 18:56:10 -0500

    Added Opening game

--- MiniMaxOpening.py
+++ MiniMaxOpening.py
@@ -1,8 +1,50 @@
 import sys
 import os
+from MoveGenerator import MoveGenerator
+import math
+
+def MaxMin(x, curr_depth):
+    if(curr_depth>=MAX_DEPTH):
+        global positions_evaluated
+        positions_evaluated+=1
+        return MoveGenerator.staticEstimation(x)
+    else:
+        d = curr_depth+1
+        v = -math.inf
+        x_children = MoveGenerator.GenerateMovesOpening(x)
+        global output_board_position
+        output_board_position = x_children[0]
+        for y in x_children:
+            v = max(v, MinMax(y, d))
+        return v
+
+def MinMax(x, curr_depth):
+    if(curr_depth>=MAX_DEPTH):
+        global positions_evaluated
+        positions_evaluated+=1
+        return MoveGenerator.staticEstimation(x)
+    else:
+        d = curr_depth+1
+        v = math.inf
+        x_children = MoveGenerator.GenerateMovesOpening(x)
+        global output_board_position
+        output_board_position = x_children[0]
+        for y in x_children:
+            v = min(v, MaxMin(y, d))
+        return v
+
+
 
 if __name__ == "__main__":
-    input_file = open(sys.argv[1])
-    input_board = input_file.readline().strip()
-    with open(sys.argv[2], 'w') as f:
-        f.write('WxWWxWWWWBBBBBBBBxxxxx')
+    positions_evaluated = 0
+    output_board_position = []
+    MAX_DEPTH = 2
+    input_board = open(sys.argv[1]).readline().strip()
+    if(sys.argv[3]):
+        MAX_DEPTH = int(sys.argv[3])
+    estimate = MaxMin(list(input_board), 0)
+
+print(f'Board Position: {"".join(output_board_position)}')
+print(f'Positions evaluated by static estimation: {positions_evaluated}.')
+print(f'MINIMAX Opening estimate: {estimate}')
+
--- MoveGenerator.py
+++ MoveGenerator.py
@@ -1,76 +1,79 @@
-from copy import deepcopy as copy
+from copy import copy
 
 class MoveGenerator:
-    def __init__(self, input_board_string):
-        self.board = input_board_string.split()
-
-    def GenerateMovesOpening(self):
-        return self.GenerateAdd()
+   
+    @staticmethod
+    def GenerateMovesOpening(board):
+        return MoveGenerator.GenerateAdd(board)
     
-    def GenerateMovesMidgameEndgame(self):
+    @staticmethod
+    def GenerateMovesMidgameEndgame(board):
         L = list()
-        if(self.board.count('W') == 3):
-            return self.GenerateHopping()
+        if(board.count('W') == 3):
+            return MoveGenerator.GenerateHopping()
         else:
-            return self.GenerateMove()
+            return MoveGenerator.GenerateMove()
 
-    def GenerateAdd(self):
+    @staticmethod
+    def GenerateAdd(board):
         L = list()
-        for location, _ in enumerate(self.board):
-            if(self.board[location]=='x'):
-                b = copy(self.board)
+        for location, _ in enumerate(board):
+            if(board[location]=='x'):
+                b = copy(board)
                 b[location] = 'W'
-                if self.closeMill(location, b):
-                    L = self.GenerateRemove(b, L)
+                if MoveGenerator.closeMill(location, b):
+                    L = MoveGenerator.GenerateRemove(b, L)
                 else:
-                    L.extend(b)
+                    L.append(b)
         return L
 
-
-    def GenerateHopping(self):
+    @staticmethod
+    def GenerateHopping(board):
         L = list()
-        for location_alpha, _ in enumerate(self.board):
-            if(self.board[location_alpha] == "W"):
-                for location_beta, _ in enumerate(self.board):
-                    if(self.board[location_beta]=='x'):
-                        b = copy(self.board)
+        for location_alpha, _ in enumerate(board):
+            if(board[location_alpha] == "W"):
+                for location_beta, _ in enumerate(board):
+                    if(board[location_beta]=='x'):
+                        b = copy(board)
                         b[location_alpha] = 'x'
                         b[location_beta] = 'W'
-                        if (self.closeMill(location_beta, b)):
-                            L = self.GenerateRemove(b,L)
+                        if (MoveGenerator.closeMill(location_beta, b)):
+                            L = MoveGenerator.GenerateRemove(b,L)
                         else:
-                            L.extend(b)
+                            L.append(b)
         return L
 
-
-    def GenerateMove(self):
+    @staticmethod
+    def GenerateMove(board):
         L = list()
-        for location, _ in enumerate(self.board):
-            if(self.board[location]=='W'):
-                n = self.neighbors(location)
+        for location, _ in enumerate(board):
+            if(board[location]=='W'):
+                n = MoveGenerator.neighbors(location)
                 for j in n:
-                    if self.board[j] == 'x':
-                        b = copy(self.board)
+                    if board[j] == 'x':
+                        b = copy(board)
                         b[location] = 'x'
                         b[j] = 'W'
-                        if self.closeMill(j,b):
-                            L = self.GenerateRemove(b, L)
+                        if MoveGenerator.closeMill(j,b):
+                            L = MoveGenerator.GenerateRemove(b, L)
                         else:
-                            L.extend(b)
+                            L.append(b)
         return L
-
-    def GenerateRemove(self, board, L):
+    
+    @staticmethod
+    def GenerateRemove(board, L):
         for location,_ in enumerate(board):
             if(board[location]=='B'):
-                if( not self.closeMill(location, board)):
+                if( not MoveGenerator.closeMill(location, board)):
                     b = copy(board)
                     b[location] = 'x'
-                    L.extend(b)
+                    L.append(b)
         if(len(L)==0):
-            L.extend(board)
+            L.append(board)
         return L
 
-    def neighbors(self, location):
+    @staticmethod
+    def neighbors(location):
         neighbors_dict = {
             0: [19, 3, 1],
             1: [0, 4, 2],
@@ -98,81 +101,84 @@ class MoveGenerator:
 
         return neighbors_dict[location]
     
-    def __comparePieces(self,board, i,j,C):
+    @staticmethod
+    def __comparePieces(board, i,j,C):
         return board[i]==C and board[j]==C
-
-    def closeMill(self, location, b):
+    
+    @staticmethod
+    def closeMill(location, b):
         C = b[location]
         if(C =='W' or C == 'B'):
             if(location == 0):
-                if(self.__comparePieces(b, 1, 2, C) or self.__comparePieces(b, 3, 6, C)):
+                if(MoveGenerator.__comparePieces(b, 1, 2, C) or MoveGenerator.__comparePieces(b, 3, 6, C)):
                     return True
             elif(location == 1):
-                if(self.__comparePieces(b, 0, 2, C)):
+                if(MoveGenerator.__comparePieces(b, 0, 2, C)):
                     return True
             elif(location == 2):
-                if(self.__comparePieces(b, 0, 1, C) or self.__comparePieces(b, 5, 7, C) or self.__comparePieces(b, 12, 21, C)):
+                if(MoveGenerator.__comparePieces(b, 0, 1, C) or MoveGenerator.__comparePieces(b, 5, 7, C) or MoveGenerator.__comparePieces(b, 12, 21, C)):
                     return True
             elif(location == 3):
-                if(self.__comparePieces(b, 0, 6, C) or self.__comparePieces(b, 4, 5, C) or  self.__comparePieces(b, 8, 16, C)):
+                if(MoveGenerator.__comparePieces(b, 0, 6, C) or MoveGenerator.__comparePieces(b, 4, 5, C) or  MoveGenerator.__comparePieces(b, 8, 16, C)):
                     return True
             elif(location == 4):
-                if(self.__comparePieces(b, 3,5, C)):
+                if(MoveGenerator.__comparePieces(b, 3,5, C)):
                     return True
             elif(location == 5):
-                if(self.__comparePieces(b, 2, 7, C) or self.__comparePieces(b, 3, 4, C) or self.__comparePieces(b, 11, 18)):
+                if(MoveGenerator.__comparePieces(b, 2, 7, C) or MoveGenerator.__comparePieces(b, 3, 4, C) or MoveGenerator.__comparePieces(b, 11, 18, C)):
                     return True
             elif(location == 6):
-                if(self.__comparePieces(b, 0, 3, C) or self.__comparePieces(b, 9, 13, C)):
+                if(MoveGenerator.__comparePieces(b, 0, 3, C) or MoveGenerator.__comparePieces(b, 9, 13, C)):
                     return True
             elif(location == 7):
-                if(self.__comparePieces(b, 5, 2, C) or self.__comparePieces(b, 10, 15, C)):
+                if(MoveGenerator.__comparePieces(b, 5, 2, C) or MoveGenerator.__comparePieces(b, 10, 15, C)):
                     return True
             elif(location == 8):
-                if(self.__comparePieces(b, 3, 16, C)):
+                if(MoveGenerator.__comparePieces(b, 3, 16, C)):
                     return True
             elif(location == 9):
-                if(self.__comparePieces(b, 6, 13, C)):
+                if(MoveGenerator.__comparePieces(b, 6, 13, C)):
                     return True
             elif(location == 10):
-                if(self.__comparePieces(b, 7, 15, C) or self.__comparePieces(b, 11, 12, C)):
+                if(MoveGenerator.__comparePieces(b, 7, 15, C) or MoveGenerator.__comparePieces(b, 11, 12, C)):
                     return True
             elif(location == 11):
-                if(self.__comparePieces(b, 10, 12, C) or self.__comparePieces(b, 5, 18, C)):
+                if(MoveGenerator.__comparePieces(b, 10, 12, C) or MoveGenerator.__comparePieces(b, 5, 18, C)):
                     return True
             elif(location == 12):
-                if(self.__comparePieces(b, 10, 11, C) or self.__comparePieces(b, 2, 21, C)):
+                if(MoveGenerator.__comparePieces(b, 10, 11, C) or MoveGenerator.__comparePieces(b, 2, 21, C)):
                     return True
             elif(location == 13):
-                if(self.__comparePieces(b, 9, 6, C) or self.__comparePieces(b, 16, 19, C) or  self.__comparePieces(b, 14, 15, C)):
+                if(MoveGenerator.__comparePieces(b, 9, 6, C) or MoveGenerator.__comparePieces(b, 16, 19, C) or  MoveGenerator.__comparePieces(b, 14, 15, C)):
                     return True
             elif(location == 14):
-                if(self.__comparePieces(b, 13, 15, C) or self.__comparePieces(b, 17, 20, C)):
+                if(MoveGenerator.__comparePieces(b, 13, 15, C) or MoveGenerator.__comparePieces(b, 17, 20, C)):
                     return True
             elif(location == 15):
-                if(self.__comparePieces(b, 10, 7, C) or self.__comparePieces(b, 13, 14, C) or  self.__comparePieces(b, 18, 21, C)):
+                if(MoveGenerator.__comparePieces(b, 10, 7, C) or MoveGenerator.__comparePieces(b, 13, 14, C) or  MoveGenerator.__comparePieces(b, 18, 21, C)):
                     return True
             elif(location == 16):
-                if(self.__comparePieces(b, 13, 19, C) or self.__comparePieces(b, 3, 8, C) or  self.__comparePieces(b, 17, 18, C)):
+                if(MoveGenerator.__comparePieces(b, 13, 19, C) or MoveGenerator.__comparePieces(b, 3, 8, C) or  MoveGenerator.__comparePieces(b, 17, 18, C)):
                     return True
             elif(location == 17):
-                if(self.__comparePieces(b, 14, 20, C) or self.__comparePieces(b, 16, 18, C)):
+                if(MoveGenerator.__comparePieces(b, 14, 20, C) or MoveGenerator.__comparePieces(b, 16, 18, C)):
                     return True
             elif(location == 18):
-                if(self.__comparePieces(b, 15, 21, C) or self.__comparePieces(b, 11, 5, C) or  self.__comparePieces(b, 16, 17, C)):
+                if(MoveGenerator.__comparePieces(b, 15, 21, C) or MoveGenerator.__comparePieces(b, 11, 5, C) or  MoveGenerator.__comparePieces(b, 16, 17, C)):
                     return True
             elif(location == 19):
-                if(self.__comparePieces(b, 16, 13, C) or self.__comparePieces(b, 20, 21, C)):
+                if(MoveGenerator.__comparePieces(b, 16, 13, C) or MoveGenerator.__comparePieces(b, 20, 21, C)):
                     return True
             elif(location == 20):
-                if(self.__comparePieces(b, 19, 21, C) or self.__comparePieces(b, 17, 14, C)):
+                if(MoveGenerator.__comparePieces(b, 19, 21, C) or MoveGenerator.__comparePieces(b, 17, 14, C)):
                     return True
             elif(location == 21):
-                if(self.__comparePieces(b, 19, 20, C) or self.__comparePieces(b, 15, 18, C) or  self.__comparePieces(b, 12, 2, C)):
+                if(MoveGenerator.__comparePieces(b, 19, 20, C) or MoveGenerator.__comparePieces(b, 15, 18, C) or  MoveGenerator.__comparePieces(b, 12, 2, C)):
                     return True
         return False
 
-    def staticEstimation(self, b, mode='opening'):
+    @staticmethod
+    def staticEstimation(b, mode='opening'):
         numWhitePieces = b.count('W')
         numBlackPieces = b.count('B')
         numBlackMoves = 0
